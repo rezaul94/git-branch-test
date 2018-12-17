a010003dd50d41f956135ebfb62d588289790ff7
Rezaul Huq <rezaulhuq94@gmail.com>
2018-12-17 13:57:12 +0600

    Added main function

--- src/Haha.java
+++ src/Haha.java
@@ -3,4 +3,8 @@ public class Haha {
     public String toString() {
         return super.toString();
     }
+
+    public static void main(String[] args) {
+        System.out.println("nothing");
+    }
 }
