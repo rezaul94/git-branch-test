73b070af16c7c7aeedadc38fcb7c8753e0cf267b
Rezaul Huq <rezaulhuq94@gmail.com>
2018-12-17 13:59:27 +0600

    removed overide

--- src/Haha.java
+++ src/Haha.java
@@ -1,7 +1,5 @@
 public class Haha {
-    @Override
-    public String toString() {
-        return super.toString();
+    public static void main(String[] args) {
+        System.out.println("nothing");
     }
-    
 }
