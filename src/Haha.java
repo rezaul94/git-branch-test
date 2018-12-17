b4646c5868abf2df1e6cf5f7519551127693a52f
Rezaul Huq <rezaulhuq94@gmail.com>
2018-12-17 14:07:01 +0600

    overide again

--- src/Haha.java
+++ src/Haha.java
@@ -1,4 +1,9 @@
 public class Haha {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println("nothing");
     }
